7035ec05c5ab576076f0add20a2f8969e8f4e9ae
mari <mariana.ferroni@estudante.ifms.edu.br>
2023-09-11 22:11:16 -0400

    armazenando nome no localStorage

--- src/app/functions/handlerAcess.js
+++ src/app/functions/handlerAcess.js
@@ -10,6 +10,7 @@ const handlerAcessUser = async (user) => {
 
     if (isTokenValidate) {
         Cookies.set('token', userAuth.token, { expires: 1 });
+        localStorage.setItem('name', userAuth.name)
     }
     return userAuth;
 }
