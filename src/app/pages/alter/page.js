c9024b87a2a2f14a821d2c490b5dbb5e1060e9e8
mari <mariana.ferroni@estudante.ifms.edu.br>
2023-09-11 23:14:35 -0400

    toastfy adicionado

--- src/app/pages/alter/page.js
+++ src/app/pages/alter/page.js
@@ -4,19 +4,23 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 export default async function Alter() {
+    const alterForm = (e) => {
+        e.preventDefault();
+        toast.success("Usuário alterado com sucesso!!")
+    }
     return (
         <div>
             <h1>Alterar Usuário</h1>
             <div className="form">
-                <form action="">
+                <form onSubmit={alterForm}>
                     <label>Nome:</label>
-                    <input type="text" />
+                    <input type="text" required/>
 
                     <label>E-mail:</label>
-                    <input type="email" />
+                    <input type="email" required/>
 
                     <label>Senha:</label>
-                    <input type="password" />
+                    <input type="password" required/>
 
                     <input type="submit" value="Alterar" />
                 </form>
--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -4,19 +4,24 @@ import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 
 export default async function Register() {
+    const submitForm = (e) => {
+        e.preventDefault();
+        toast.success("Usuário cadastrado com sucesso!!")
+    }
+
     return (
         <div>
             <h1>Cadastrar Usuário</h1>
             <div className="form">
-                <form action="">
+                <form onSubmit={submitForm}>
                     <label>Nome:</label>
-                    <input type="text" />
+                    <input type="text" required/>
 
                     <label>E-mail:</label>
-                    <input type="email" />
+                    <input type="email" required/>
 
                     <label>Senha:</label>
-                    <input type="password"/>
+                    <input type="password" required/>
 
                     <input type="submit" value="Cadastrar" />
                 </form>
