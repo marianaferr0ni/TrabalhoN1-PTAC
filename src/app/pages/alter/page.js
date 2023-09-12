553ba785775a6285511c452562b940edc54fc626
mari <mariana.ferroni@estudante.ifms.edu.br>
2023-09-11 22:49:58 -0400

    forms criados

--- src/app/pages/alter/page.js
+++ src/app/pages/alter/page.js
@@ -1,7 +1,27 @@
+'use client'
+import React from 'react';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 export default async function Alter() {
     return (
         <div>
             <h1>Alterar Usuário</h1>
+            <div className="form">
+                <form action="">
+                    <label>Nome:</label>
+                    <input type="text" />
+
+                    <label>E-mail:</label>
+                    <input type="email" />
+
+                    <label>Senha:</label>
+                    <input type="password" />
+
+                    <input type="submit" value="Alterar" />
+                </form>
+            </div>
+            <ToastContainer />
         </div>
     );
 };
--- src/app/pages/register/page.js
+++ src/app/pages/register/page.js
@@ -1,7 +1,27 @@
+'use client'
+import React from 'react';
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
+
 export default async function Register() {
     return (
         <div>
             <h1>Cadastrar Usuário</h1>
+            <div className="form">
+                <form action="">
+                    <label>Nome:</label>
+                    <input type="text" />
+
+                    <label>E-mail:</label>
+                    <input type="email" />
+
+                    <label>Senha:</label>
+                    <input type="password"/>
+
+                    <input type="submit" value="Cadastrar" />
+                </form>
+            </div>
+            <ToastContainer />
         </div>
     );
 };
