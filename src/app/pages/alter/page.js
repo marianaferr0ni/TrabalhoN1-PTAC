b11fda3bc3a6244ed2b5fbc9857e5059deccb244
mari <mariana.ferroni@estudante.ifms.edu.br>
2023-09-12 17:23:05 -0400

    css da pagina alter e responsivo da alter e dashboard

--- src/app/pages/alter/page.js
+++ src/app/pages/alter/page.js
@@ -2,6 +2,8 @@
 import React from 'react';
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Header from '@/app/components/Header';
+import './alter.css'
 
 export default async function Alter() {
     const alterForm = (e) => {
@@ -9,21 +11,24 @@ export default async function Alter() {
         toast.success("Usuário alterado com sucesso!!")
     }
     return (
-        <div>
+        <div className='alter'>
+            <Header />
             <h1>Alterar Usuário</h1>
-            <div className="form">
-                <form onSubmit={alterForm}>
-                    <label>Nome:</label>
-                    <input type="text" required/>
+            <div className='container'>
+                <div className="form">
+                    <form onSubmit={alterForm}>
+                        <label>Nome:</label>
+                        <input type="text" required/>
 
-                    <label>E-mail:</label>
-                    <input type="email" required/>
+                        <label>E-mail:</label>
+                        <input type="email" required/>
 
-                    <label>Senha:</label>
-                    <input type="password" required/>
+                        <label>Senha:</label>
+                        <input type="password" required/>
 
-                    <input type="submit" value="Alterar" />
-                </form>
+                        <input type="submit" value="Alterar" />
+                    </form>
+                </div>
             </div>
             <ToastContainer />
         </div>
