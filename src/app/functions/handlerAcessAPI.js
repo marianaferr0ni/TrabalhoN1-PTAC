c4a393b24aea5539a7ec2ec4a1e61d657e35b38f
mari <mariana.ferroni@estudante.ifms.edu.br>
2023-10-17 16:06:01 -0400

    login com api vercel

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,26 +1,20 @@
 'use server'
 
-const users = [
-    { name: 'Kau', email: 'kau@email.com', password: 'smilinguido', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' },
-    { name: 'Will', email: 'will@email.com', password: 'euamoavitoria', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' },
-    { name: 'Mari', email: 'mari@email.com', password: 'lalala', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' },
-    { name: 'Lucca', email: 'lucca@email.com', password: 'bomba', token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c' }
-];
+const url = "https://aula-17-10-henna.vercel.app"
 
-
-const getUserAuthenticated = (user) => {
-    let userAuth = {}
-    for (let i = 0; i < users.length; i++) {
-        if(users[i].email === user.email && users[i].password === user.password) 
-        {
-            userAuth=users[i];
-        }
-    }
-    return userAuth; 
+const getUserAuthenticated = async (user) => {
+    const responseOfApi = await fetch(url + "/user/authenticated", 
+    {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(user)
+    }); 
+    const userAuth = await responseOfApi.json();
+    return userAuth;
 } 
 
-const getUsers = (user) => {
-    return users
+const getUsers = () => {
+    
 }
 
 export { getUsers, getUserAuthenticated };
